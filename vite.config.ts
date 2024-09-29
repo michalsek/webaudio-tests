93ea0c00a846dd378dd2199a83a09bceafcc54d6
Michal Sek <michal.sek@swmansion.com>
2024-09-29 18:53:45 +0200

    one more

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // @ts-expect-error
+  base: process.env.BASE_URL || '/',
   plugins: [react()],
 });
